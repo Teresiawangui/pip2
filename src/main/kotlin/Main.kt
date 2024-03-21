289b411c2b4e244214d24a03c99669cf673cfd99
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-03-21 23:01:12 +0300

    assignments correction

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,6 +10,13 @@ fun main() {
     words(arrayOf(12,23,24,35,36))
     /*println(text("123"))
     println(reverseString("akirachix"))*/
+
+    /*did this on march 23*/
+    myFriends("jane","mercy","nancy","lucy")
+    statistics()
+    words("nanny","mary","nancy")
+
+
 }
 
 //Create a function sum that takes two parameters and returns their sum.
@@ -54,3 +61,33 @@ fun reverseString(name:String):String{
 fun words(numbers:Array<Int>){
     println(numbers.sum())
 }
+
+
+//create a function that takes in 4 strings and creates an array out of them then prints out the array.
+fun myFriends(name1:String,name2:String,name3:String,name4:String){
+    var friends = arrayOf(name1,name2,name3,name4)
+    println(friends.contentToString())
+}
+
+
+
+//create one function that is given array
+    fun statistics () {
+        var numbers = arrayOf(32, 17, 4, 213, 78, 43, 90, 31, 3, 73, 11, 158, 62)
+        print(numbers[1] + numbers[4])
+        var index =numbers.indexOf(158)
+    println(index)
+        var sortedNumbers = numbers.sortedArray()
+    println(sortedNumbers.contentToString())
+
+
+
+
+    }
+//create a function that takes in 3 names and returns a string array containing all 3 names.
+fun words(namea:String,nameb:String,namec:String):String{
+    var names = arrayOf(namea,nameb, namec)
+    return names.contentToString()
+}
+
+
